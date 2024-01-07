cd2edccb360d4f5e87743435d2df97047d554f27
Joshua Lochner <admin@xenova.com>
2024-01-07 12:40:26 +0200

    Add translation node

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@
 
 import TEXT_CLASSIFICATION_NODE from './nodes/text-classification.js';
 import IMAGE_SEGMENTATION_NODE from './nodes/image-segmentation.js';
+import TEXT2TEXT_GENERATION_NODE from './nodes/translation.js';
 
 // Register custom nodes with visual blocks to start using them.
 visualblocks.registerCustomNode(TEXT_CLASSIFICATION_NODE);
 visualblocks.registerCustomNode(IMAGE_SEGMENTATION_NODE);
+visualblocks.registerCustomNode(TEXT2TEXT_GENERATION_NODE);
---
+++ src/nodes/translation.js
@@ -0,0 +1,91 @@
+
+import { PipelineSingleton, BasePipelineNode } from './base.js';
+
+import {
+    COLLECTION_NAME,
+} from '../constants.js';
+
+const NODE_SPEC = {
+    'id': 'transformers-translation',
+    'name': 'Translation',
+    'description': 'TODO',
+
+    'category': 'processor',
+    'collection': COLLECTION_NAME,
+
+    // Properties.
+    "propertySpecs": [
+        {
+            "name": "language",
+            "type": "string",
+            "editorSpec": {
+                "type": "dropdown",
+                "options": [
+                    {
+                        "value": "French",
+                        "label": "French"
+                    },
+                    {
+                        "value": "German",
+                        "label": "German"
+                    },
+                    {
+                        "value": "Romanian",
+                        "label": "Romanian"
+                    }
+                ]
+            }
+        }
+    ],
+
+    // Inputs.
+    'inputSpecs': [{
+        'name': 'text',
+        'type': 'string',
+        "editorSpec": {
+            "type": "text_input"
+        },
+    }],
+
+    // Outputs.
+    'outputSpecs': [{
+        'name': 'result',
+        'type': 'string',
+    }],
+};
+
+class Text2TextPipelineSingleton extends PipelineSingleton {
+    static task = 'text2text-generation';
+    static modelId = 'Xenova/t5-small';
+    static quantized = true;
+}
+
+const TEXT2TEXT_GENERATION_NODE_STYLE = `
+.container {
+    width: 100%;
+    height: 100%;
+}
+`
+
+class Text2TextGenerationNode extends BasePipelineNode {
+    constructor() {
+        super(Text2TextPipelineSingleton, TEXT2TEXT_GENERATION_NODE_STYLE);
+    }
+
+
+    async runWithInputs(inputs) {
+        const { text, language } = inputs;
+        if (!text) return; // No input node
+
+        const translator = await this.instance;
+
+        const prompt = `translate English to ${language}: ${text}`;
+        const result = await translator(prompt);
+        this.root.innerHTML = JSON.stringify(result);
+
+        // Output.
+        this.dispatchEvent(new CustomEvent('outputs', { detail: { 'result': result[0].generated_text } }));
+    }
+}
+
+export default { nodeSpec: NODE_SPEC, nodeImpl: Text2TextGenerationNode };
