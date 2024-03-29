8dd9942b1d0c8d9dd0302eb2b82560f707456513
Radames Ajna <radamajna@gmail.com>
2024-03-28 23:42:13 -0700

    only processed image as output

--- src/nodes/shared/depth-estimation-viewer-spec.ts
+++ src/nodes/shared/depth-estimation-viewer-spec.ts
@@ -1,13 +1,16 @@
 import { COLLECTION_NAME_SHARED } from "../../constants";
 import { CustomDataTypeEnum } from "../../types";
-import { EditorType, DataType, Category } from "@visualblocks/custom-node-types";
+import {
+  EditorType,
+  DataType,
+  Category,
+} from "@visualblocks/custom-node-types";
 import type { NodeSpec } from "@visualblocks/custom-node-types";
 
 export const NODE_SPEC: NodeSpec = {
   id: "hf-transformers-depth-estimation-viewer",
   name: "Depth Estimation Viewer",
-  description:
-    "View the depth estimation result.",
+  description: "View the depth estimation result.",
 
   category: Category.OUTPUT,
   collection: COLLECTION_NAME_SHARED,
@@ -37,17 +40,12 @@ export const NODE_SPEC: NodeSpec = {
         min: 0,
         max: 1,
         step: 0.01,
-      }
+      },
     },
   ],
 
   // Outputs.
   outputSpecs: [
-    {
-      name: "depthData",
-      displayLabel: "Depth Data",
-      type: CustomDataTypeEnum.DEPTH_ESTIMATION_RESULT,
-    },
     {
       name: "image",
       type: DataType.IMAGE,
